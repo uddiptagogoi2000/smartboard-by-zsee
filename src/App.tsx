8c8ec11f7b34622d0b945faf1f694b7a7d1a24a2
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2024-12-19 21:26:04 +0530

    chore: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,38 @@ import system from './theme';
 import NotFound from './components/common/NotFound';
 import SignupCard from './components/common/SignupForm';
 import ComponentPlayground from './components/common/ComponentPlaygroud';
+import AuthLayout from './layouts/AuthLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from './components/ui/toaster';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ChakraProvider value={system}>
-      <ColorModeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/signup' element={<SignupCard />} />
-            <Route path='/' element={<RootLayout />}>
-              <Route index element={<NotFound />} />
-              <Route path='/components' element={<ComponentPlayground />} />
-              <Route path='*' element={<NotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ColorModeProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider value={system}>
+        <ColorModeProvider>
+          <Toaster />
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path='/signup'
+                element={
+                  <AuthLayout>
+                    <SignupCard />
+                  </AuthLayout>
+                }
+              />
+              <Route path='/' element={<RootLayout />}>
+                <Route index element={<NotFound />} />
+                <Route path='/components' element={<ComponentPlayground />} />
+                <Route path='*' element={<NotFound />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ColorModeProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/components/common/SignupForm.tsx
+++ src/components/common/SignupForm.tsx
@@ -1,153 +1,201 @@
 import {
-  Box,
   Button,
-  Container,
   Flex,
+  Grid,
+  GridItem,
   Heading,
-  HStack,
   Input,
   Link,
   Stack,
   Text,
 } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { RiArtboard2Line } from '@remixicon/react';
-import BgPattern from '../../assets/bg-pattern.svg';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { signupSchema } from '../../schemas';
+import { calculatePasswordStrength, capitalizeFirstLetter } from '../../utils';
 import { Field } from '../ui/field';
 import { PasswordInput, PasswordStrengthMeter } from '../ui/password-input';
-import { ColorModeProvider } from '../ui/color-mode';
+import { toaster } from '../ui/toaster';
+import useAuth from '../../hooks/auth/useAuth';
 
-function SignupCard() {
+type FormData = yup.InferType<typeof signupSchema>;
+
+const signupToasterMessages = {
+  success: {
+    title: 'Signup Successful',
+    description: 'Your account has been created successfully.',
+  },
+  error: {
+    title: 'Signup Failed',
+    description:
+      'There was an issue with creating your account. Please try again.',
+  },
+  loading: {
+    title: 'Processing Signup',
+    description: 'Please wait while we complete your registration.',
+  },
+};
+
+function SignupForm() {
+  const { signUp } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FormData>({
+    resolver: yupResolver(signupSchema, { abortEarly: false }),
+  });
+
+  const password = watch('password', '');
+  const passwordStrength = calculatePasswordStrength(password);
+
+  const onSubmit = ({
+    email,
+    password,
+    firstName,
+    lastName = '',
+  }: FormData) => {
+    toaster.promise(
+      signUp({
+        email,
+        password,
+        firstName,
+        lastName,
+      }),
+      signupToasterMessages
+    );
+  };
+
   return (
-    <ColorModeProvider forcedTheme='light'>
-      <Box
-        minH={'100vh'}
-        position={'relative'}
-        bg={{ base: 'brand.lightestgrey', _dark: 'brand.darkestblue' }}
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      <Stack
+        align={{
+          sm: 'center',
+          md: 'flex-start',
+        }}
+        pb={8}
       >
-        <Box
-          position={'absolute'}
-          top={0}
-          right={0}
-          bottom={0}
-          left={0}
-          bgImage={`url(${BgPattern})`}
-          bgSize={'contain'}
-          bgPos={'left 0 top -40%'}
-          bgRepeat={'no-repeat'}
-          opacity={1}
-        />
-
-        <Box pt={3}>
-          <Container>
-            <Flex alignItems={'center'}>
-              <RiArtboard2Line size={24} />
-              <Heading ml={1} size={'xl'} fontWeight={'light'}>
-                SmartBoard
-              </Heading>
-            </Flex>
-          </Container>
-        </Box>
-        <Box>
-          <Stack mx={'auto'} maxW={'lg'} py={12} px={6} position={'relative'}>
-            <Box
-              rounded={'lg'}
-              boxShadow={'lg'}
-              p={8}
-              bg={{ base: 'white', _dark: 'brand.layerdark' }}
+        <Flex
+          smDown={{
+            mx: 'auto',
+          }}
+        >
+          <RiArtboard2Line size={24} />
+          <Heading ml={1} size={'xl'} fontWeight={'light'}>
+            SmartBoard
+          </Heading>
+        </Flex>
+        <Heading fontSize={'2xl'} textAlign={'center'}>
+          Create an account
+        </Heading>
+        <Text fontSize={'md'} color={{ base: 'gray.600' }}>
+          Start your IoT project with SmartBoard
+        </Text>
+      </Stack>
+      <Stack gap={4}>
+        <Grid
+          alignItems={'flex-start'}
+          templateColumns='1fr'
+          md={{
+            gridTemplateColumns: 'repeat(2, 1fr)',
+          }}
+          gap={'4'}
+        >
+          <GridItem>
+            <Field
+              label='First Name'
+              errorText={capitalizeFirstLetter(errors.firstName?.message)}
+              invalid={!!errors.firstName}
+              required
             >
-              <Stack align={'flex-start'} pb={8}>
-                <Flex alignItems={'center'}>
-                  <RiArtboard2Line size={24} />
-                  <Heading ml={1} size={'xl'} fontWeight={'light'}>
-                    SmartBoard
-                  </Heading>
-                </Flex>
-                <Heading fontSize={'2xl'} textAlign={'center'}>
-                  Create an account
-                </Heading>
-                <Text
-                  fontSize={'md'}
-                  color={{ base: 'gray.600', _dark: 'whiteAlpha.700' }}
-                >
-                  Start your IoT project with SmartBoard
-                </Text>
-              </Stack>
-              <Stack gap={4}>
-                <HStack>
-                  <Box>
-                    <Field
-                      label='First Name'
-                      required
-                      helperText='This field is required'
-                    >
-                      <Input placeholder='Enter your first name' />
-                    </Field>
-                  </Box>
-                  <Box>
-                    <Field
-                      label='Last Name'
-                      required
-                      helperText='This field is required'
-                    >
-                      <Input placeholder='Enter your last name' />
-                    </Field>
-                  </Box>
-                </HStack>
-                <Field
-                  label='Email'
-                  required
-                  helperText='This field is required'
-                >
-                  <Input
-                    type='email'
-                    placeholder='Enter your email'
-                    colorPalette={'white'}
-                  />
-                </Field>
+              <Input
+                placeholder='Enter your first name'
+                {...register('firstName')}
+              />
+            </Field>
+          </GridItem>
+          <GridItem>
+            <Field
+              label='Last Name'
+              errorText={capitalizeFirstLetter(errors.lastName?.message)}
+              invalid={!!errors.lastName}
+            >
+              <Input
+                placeholder='Enter your last name'
+                {...register('lastName')}
+              />
+            </Field>
+          </GridItem>
+        </Grid>
+        <Field
+          label='Email'
+          errorText={capitalizeFirstLetter(errors.email?.message)}
+          invalid={!!errors.email}
+          required
+        >
+          <Input
+            type='email'
+            placeholder='Enter your email'
+            colorPalette={'white'}
+            formNoValidate
+            {...register('email')}
+          />
+        </Field>
 
-                <Field
-                  label='Password'
-                  required
-                  helperText='This field is required'
-                >
-                  <Stack width={'full'}>
-                    <PasswordInput />
-                    <PasswordStrengthMeter value={2} />
-                  </Stack>
-                </Field>
-
-                <Field
-                  label='Confirm Password'
-                  required
-                  helperText='This field is required'
-                >
-                  <Stack width={'full'}>
-                    <PasswordInput />
-                  </Stack>
-                </Field>
-                <Stack gap={10} pt={2}>
-                  <Button
-                    size='lg'
-                    colorPalette={'teal'}
-                    bgGradient='to-r'
-                    gradientFrom={'teal.400'}
-                    gradientTo={'teal.600'}
-                  >
-                    Sign up
-                  </Button>
-                </Stack>
-                <Stack pt={4}>
-                  <Text textAlign={'center'} fontSize={'sm'}>
-                    Already a user? <Link colorPalette={'teal'}>Login</Link>
-                  </Text>
-                </Stack>
-              </Stack>
-            </Box>
+        <Field
+          label='Password'
+          errorText={capitalizeFirstLetter(errors.password?.message)}
+          invalid={!!errors.password}
+          required
+        >
+          <Stack width={'full'}>
+            <PasswordInput
+              type='password'
+              placeholder='Enter your password'
+              {...register('password')}
+            />
+            <PasswordStrengthMeter value={passwordStrength} />
           </Stack>
-        </Box>
-      </Box>
-    </ColorModeProvider>
+        </Field>
+
+        <Field
+          label='Confirm Password'
+          errorText={capitalizeFirstLetter(errors.confirmPassword?.message)}
+          invalid={!!errors.confirmPassword}
+          required
+        >
+          <Stack width={'full'}>
+            <PasswordInput
+              type='password'
+              placeholder='Re-enter your password'
+              {...register('confirmPassword')}
+            />
+          </Stack>
+        </Field>
+        <Stack gap={10} pt={2}>
+          <Button
+            size='lg'
+            colorPalette={'teal'}
+            bgGradient='to-r'
+            gradientFrom={'teal.400'}
+            gradientTo={'teal.600'}
+            type='submit'
+          >
+            Sign up
+          </Button>
+        </Stack>
+        <Stack pt={4}>
+          <Text textAlign={'center'} fontSize={'sm'}>
+            Already a user? <Link colorPalette={'teal'}>Login</Link>
+          </Text>
+        </Stack>
+      </Stack>
+    </form>
   );
 }
 
-export default SignupCard;
+export default SignupForm;
