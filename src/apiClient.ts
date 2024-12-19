f0e947088ef8f12464ef4e0fd372389773362725
uddiptagogoi2000 <uddiptagogoi2000@gmail.com>
2024-12-19 21:23:07 +0530

    setup api client axios, add services

---
+++ src/apiClient.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export type ApiMethod = 'get' | 'post' | 'put' | 'delete';
+
+export const apiCall = <TPayload, TResponse>(
+  method: ApiMethod,
+  url: string,
+  payload?: TPayload
+): Promise<AxiosResponse<TResponse>> => {
+  return api.request<TResponse>({
+    method,
+    url,
+    data: payload,
+  });
+};
---
+++ src/services/authService.ts
@@ -0,0 +1,55 @@
+import { apiCall } from '../apiClient';
+
+export type SignInResponse = {
+  statusCode: number;
+  data: {
+    user: {
+      firstName: string;
+      lastName: string;
+      email: string;
+    };
+    accessToken: string;
+  };
+  message: string;
+};
+
+export type SignUpResponse = {
+  statusCode: number;
+  data: {
+    user: {
+      firstName: string;
+      lastName: string;
+      email: string;
+    };
+  };
+  message: string;
+};
+
+export type SignInPayload = {
+  email: string;
+  password: string;
+};
+
+export type SignUpPayload = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+const AuthApiService = {
+  signUp: (payload: SignUpPayload) =>
+    apiCall<SignUpPayload, SignUpResponse>('post', '/users/register', payload),
+
+  signIn: (payload: SignInPayload) =>
+    apiCall<SignInPayload, SignInResponse>('post', '/users/login', payload),
+
+  renewToken: () =>
+    apiCall<null, { accessToken: string }>('get', '/refresh-token'),
+
+  signOut: () => {},
+  resetPassword: () => {},
+  updatePassword: () => {},
+};
+
+export default AuthApiService;
